a737196fb83624a24db0c4939e58fb41138523c7
Alex Thoennes <Alex@Alexanders-MacBook-Pro.local>
2016-09-24 12:59:03 -0400

    Interface for Hashtable
    Hashtable interface that outlines the operations to perform.

---
+++ HashtableInterface.java
@@ -0,0 +1,11 @@
+public interface HashtableInterface
+{
+	// insert a value into the hashtable
+	void insert(int value);
+	
+	// delete a value in the hastable
+	void delete(int value);
+	
+	//display everything in the hashtable
+	void dumpHash();
+}

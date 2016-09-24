edbd0052a5f8b97e7b8ef3cbaacb4a933245c096
Alex Thoennes <Alex@Alexanders-MacBook-Pro.local>
2016-09-24 12:58:22 -0400

    Linked List Class
    The way we were told to construct this hashtable was that it is
    supposed to be an array with linked lists hanging off of it.

---
+++ LinkedList.java
@@ -0,0 +1,18 @@
+public class LinkedList
+{
+	public int value;
+	
+	public LinkedList next;
+	
+	/**
+	 * constructor for a linked list which
+	 * also sets the value
+	 * 
+	 * @param value
+	 */
+	public LinkedList(int value)
+	{
+		this.value = value;
+		this.next = null;
+	}
+}
